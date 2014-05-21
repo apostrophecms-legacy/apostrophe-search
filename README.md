8d2feabccf0649346bdc8cca91544ff4040e80d8
Tom Boutell <tom@punkave.com>
2014-05-21 16:23:56 -0400

    Search is now its own module

---
+++ README.md
@@ -0,0 +1,75 @@
+# apostrophe-search
+
+This module adds a user interface for sitewide search to A2 sites.
+
+## Installation
+
+npm install apostrophe-search
+
+## Activating the Module
+
+In `app.js`, in your `modules` section:
+
+```javascript
+    apostrophe-search: {}
+```
+
+## Adding the mini-form to your layout
+
+Just use a cross-module include:
+
+```markup
+{% include "search:miniForm.html" %}
+```
+
+## Adding the search results page to your site
+
+It's already there. Currently the `apostrophe:reset` task creates one and gives it the slug `/search`.
+
+## Customizing the search results page
+
+Create `lib/modules/apostrophe-search/views` and copy `index.html` from this module's `views` folder to that folder. Then edit as you see fit. Your override will automatically be recognized. It works just like overriding the blog module, snippets module, et cetera.
+
+## Customizing the mini-form
+
+Copy `miniForm.html` in the same way. Or just write your own form directly in your layout. You must use the GET method and the user's search text must be in the `q` query parameter. The action of your form should be `/search`.
+
+## Customizing the Search Filters
+
+By default filters are displayed which can be used to filter out various results such as blog posts, events, etc. until the user is left with the results they wanted.
+
+By default the list is very complete. The search module emits an `addSearchFilters` event on the `apos` object, passing an array as the first argument, and modules that are interested in having a search filter simply
+push page type names onto that array.
+
+If you wish to restrict the list of filters on your site you can pass a `searchFilters` option when configuring the module in `app.js`. Here the only filters provided are for blog posts and "pages."
+
+Note that the filter with the name "other" actually matches everything not matched by your other filters. We label this filter "pages" because it is less confusing for the user.
+
+```javascript
+    apostrophe-search: {
+      searchFilters: [
+        {
+          name: 'other',
+          label: 'Pages'
+        }
+        {
+          name: 'blogPost',
+          label: 'Articles'
+        }
+      ]
+    }
+```
+
+You can also shut off filters entirely for a project:
+
+```javascript
+    apostrophe-search: {
+      searchFilters: false
+    }
+```
+
+You should use `false`, not an empty array.
+
+## Styles
+
+Sample styles are available in the [sandbox project](https://github.com/punkave/apostrophe-search) in `public/css/search.less`.
---
+++ index.js
@@ -0,0 +1,202 @@
+var _ = require('lodash');
+
+module.exports = search;
+
+function search(options, callback) {
+  return new search.Search(options, callback);
+}
+
+search.Search = function(options, callback) {
+  var apos = options.apos;
+  var app = options.app;
+  var self = this;
+  self._apos = apos;
+  self._app = app;
+  self._action = '/apos-search';
+  self._perPage = options.perPage || 10;
+  self._options = options;
+
+  // Mix in the ability to serve assets and templates
+  self._apos.mixinModuleAssets(self, 'search', __dirname, options);
+
+  // A page loader function. If the page type is 'search',
+  // it'll kick in and make search results available in req.extras.search.
+  // You enable this by specifying it when you set your loader option in
+  // calling pages.serve. Include a form on the page template with itself as
+  // the action so you can make queries. (Confused? See the sandbox for
+  // an example.)
+
+  self.loader = function(req, callback) {
+
+    if (!req.page) {
+      // We're only interested in exact matches, /search/something is
+      // none of our business, we don't look at req.bestPage
+      return callback(null);
+    }
+
+    // We're only interested in enhancing pages of type "search"
+    if (req.page.type !== 'search') {
+      return callback(null);
+    }
+
+    var searchFilters = [
+      { name: 'other', label: 'Pages' }
+    ];
+
+    apos.emit('addSearchFilters', searchFilters);
+
+    // Option to override or shut off with false
+    if (self._options.searchFilters !== undefined) {
+      searchFilters = self._options.searchFilters;
+    }
+
+    req.extras.searchFilters = searchFilters;
+
+    var q = self._apos.sanitizeString(req.query.q);
+    req.extras.q = q;
+
+    var resultGroups = [];
+
+    // A simple way for modules to declare themselves unsearchable:
+    // just listen for the "unsearchable" event and push page type names
+    // onto the array it receives as its sole argument.
+
+    var unsearchable = [];
+    apos.emit('unsearchable', unsearchable);
+
+    var filtersActive = [];
+    var filtersInactive = [];
+
+
+    req.extras.filterStatus = {};
+
+    if (searchFilters) {
+      _.each(searchFilters, function(filter) {
+        var value = self._apos.sanitizeBoolean(req.query[filter.name], true);
+        if (value) {
+          filter.active = true;
+          filtersActive.push(filter.name);
+          req.extras.filterStatus[filter.name] = '';
+        } else {
+          filtersInactive.push(filter.name);
+          req.extras.filterStatus[filter.name] = '0';
+        }
+      });
+    }
+
+    var criteria = {};
+
+    var $in;
+    var $nin;
+    var typeCriteria = {};
+
+    // If all filters are active, we don't have
+    // to do anything, otherwise...
+    if (filtersInactive.length) {
+      if (!_.contains(filtersActive, 'other'))  {
+        typeCriteria.$in = filtersActive;
+      } else {
+        typeCriteria.$nin = _.filter(filtersInactive, function(name) { return name !== 'other';
+        });
+      }
+    }
+    if (unsearchable.length) {
+      typeCriteria.$nin = (typeCriteria.$nin || []).concat(unsearchable);
+    }
+    if (typeCriteria.$in || typeCriteria.$nin) {
+      criteria.type = typeCriteria;
+    }
+
+    apos.emit('addSearchCriteria', req, criteria);
+
+    var options = {};
+
+    self.addPager(req, options);
+
+    var sort;
+    if ((!req.query.sort) || (req.query.sort === 'quality')) {
+      sort = 'q';
+    } else {
+      // Chronological sort
+      sort = { start: -1, publishedAt: -1, createdAt: -1 };
+    }
+
+    options.sort = sort;
+
+    options.search = q;
+
+    self.addPager(req, options);
+
+    return self._apos.get(req, criteria, options, function(err, results) {
+      if (err) {
+        console.error(err);
+        req.statusCode = 500;
+        return callback(null);
+      }
+      self.setPagerTotal(req, results.total);
+      req.extras.search = results.pages;
+      req.template = self.renderer('index');
+      return callback(null);
+    });
+  };
+
+  // Sets up req.extras.pager and adds skip and limit to the criteria.
+  // We also setPagerTotal after the total number of items available
+  // is known (results.total in the get callback). Also sets an appropriate
+  // limit if an RSS feed is to be generated.
+
+  self.addPager = function(req, options) {
+    var pageNumber = self._apos.sanitizeInteger(req.query.page, 1, 1);
+    req.extras.pager = {
+      page: pageNumber
+    };
+
+    options.skip = self._perPage * (pageNumber - 1);
+    options.limit = self._perPage;
+  };
+
+  self.setPagerTotal = function(req, total) {
+    if (req.extras.pager) {
+      req.extras.pager.total = Math.ceil(total / self._perPage);
+      if (req.extras.pager.total < 1) {
+        req.extras.pager.total = 1;
+      }
+    }
+  };
+
+  // Given a slug that was returned as a search result, generate a redirect
+  // to the appropriate place. The idea is that doing this when users actually
+  // click is much cheaper than determining the perfect URL for every search
+  // result in the list, most of which will never be clicked on
+
+  app.get(self._action + '/search-result', function(req, res) {
+    var slug = req.query.slug;
+    return apos.getPage(req, slug, function(err, page) {
+      if (!page) {
+        res.statusCode = 404;
+        return res.send('Not Found');
+      }
+      if (page.slug.match(/\//)) {
+        // TODO this is another place we are hardcoding the root, it is
+        // increasingly clear we don't support more than one root right now
+        return res.redirect(page.slug);
+      } else {
+        // we don't know what to do with this kind of page, but
+        // another module might; emit an event
+        var context = {};
+        apos.emit('searchResult', req, res, page, context);
+        if (!context.accepted) {
+          // No one will admit to knowing what to do with this page
+          res.statusCode = 404;
+          return res.send('Not Found');
+        } else {
+          // Someone else is asynchronously dealing with it, we're good here
+        }
+      }
+    });
+  });
+  if (callback) {
+    return process.nextTick(callback);
+  }
+};
+
